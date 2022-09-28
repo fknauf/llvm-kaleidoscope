9abb3bf83732d302b21d5c5cdcb9d6f3511123c5
Florian Knauf <knauf@vss.uni-hannover.de>
2022-09-28 15:17:28 +0200

    API-Funktionen mit __attribute__((retain))

--- src/kaleidoscope/api_functions.cpp
+++ src/kaleidoscope/api_functions.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
 
-extern "C" double putchard(double X)
+#ifdef _WIN32
+#define DLLEXPORT __declspec(dllexport)
+#else
+#define DLLEXPORT __attribute__((retain))
+#endif
+
+extern "C" double DLLEXPORT putchard(double X)
 {
     std::cerr << static_cast<char>(X) << std::flush;
     return 0;
 }
 
 /// printd - printf that takes a double prints it as "%f\n", returning 0.
-extern "C" double printd(double X)
+extern "C" double DLLEXPORT printd(double X)
 {
     std::cerr << X << std::endl;
     return 0;
